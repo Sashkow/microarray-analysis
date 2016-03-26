89a0bdca644f38317cd7a6ceffb75542e74305c1
Sashkow <lykhenko.olexandr@gmail.com>
2016-03-27 00:37:39 +0200

    builds geneToExpressionValueMapping i.e. maps gene entrezid to chip expression value for each dataset

--- arrayQualityMetricsLumi.R
+++ arrayQualityMetricsLumi.R
@@ -11,25 +11,70 @@ fileName1 = "GSE30186_non_normalized.txt"
 
 lumiBatch = lumiR.batch(fileName1, sampleInfoFile = "sampleInfo.txt")
 
-length(lumiBatch@assayData$exprs)
-
-limiProcessedBatch = lumiExpresso(lumiBatch)
-
-
+lumiProcessedBatch = lumiExpresso(lumiBatch)
 
 arrayQualityMetrics::arrayQualityMetrics(expressionset = limiProcessedBatch,
                                          outdir = "QC_lumi_processesd",
                                          force = TRUE,
                                          do.logtransform = FALSE,
                                          intgroup = "Target")
-
-
 summary(limiProcessedBatch, "QC")
 
+lumi_probesetid_list = featureNames(lumiProcessedBatch)
+length(lumi_probesetid_list) # 47318
 
-lumi_probesetid_list = featureNames(limiProcessedBatch)
+probeid_to_gene_mapping = select(illuminaHumanv4.db, lumi_probesetid_list, "ENTREZID") 
+length(probeid_to_gene_mapping$PROBEID) # 50627 != 47318
+length(unique(probeid_to_gene_mapping$PROBEID)) # 47318
 
-probeid_to_gene_mapping = select(illuminaHumanv4.db, lumi_probesetid_list, "ENTREZID")
+
+
+# exclude probesets that do not map on genes
+probeid_to_gene_mapping = probeid_to_gene_mapping[!is.na(probeid_to_gene_mapping$ENTREZID),] # 39191
+# exclude probestts that map on multiple gnes
+duplicated_probeid = unique(probeid_to_gene_mapping[duplicated(probeid_to_gene_mapping$PROBEID),]$PROBEID) # amount of unique probeids that have duplicaties 1726
+probeid_to_gene_mapping = probeid_to_gene_mapping[!(probeid_to_gene_mapping$PROBEID %in% duplicated_probeid),] # 34156
+# separate probesets that map on unique gene and those that map on multiple genes
+unique_entrezid = unique(probeid_to_gene_mapping[duplicated(probeid_to_gene_mapping$ENTREZID),]$ENTREZID) # 7703
+probeid_to_gene_mapping_duponly = probeid_to_gene_mapping[probeid_to_gene_mapping$ENTREZID %in% unique_entrezid,] # 20818
+probeid_to_gene_mapping_no_dupl = probeid_to_gene_mapping[!(probeid_to_gene_mapping$ENTREZID %in% unique_entrezid),] # 13338
+
+
+lumiBatchFrame = data.frame(lumiProcessedBatch@assayData$exprs)
+geneToExpressionValueMappingDuplicate = lumiBatchFrame[0,] # empty data frame with same column
+geneToExpressionValueMappingNoDuplicate = data.frame()
+lumiBatchFrame$PROBEID <- rownames(lumiBatchFrame)
+
+# merge probeid_to_gene_mapping_no_dupl and lumiBatchFrame by probesetid
+ha = probeid_to_gene_mapping_no_dupl
+hb = lumiBatchFrame
+geneToExpressionValueMappingNoDuplicate = merge(ha, hb)
+geneToExpressionValueMappingNoDuplicate$PROBEID = NULL
+length(colnames(geneToExpressionValueMappingNoDuplicate))
+
+
+for (gene_name in unique_entrezid){
+  probesetidList = probeid_to_gene_mapping_duponly[probeid_to_gene_mapping_duponly$ENTREZID == gene_name,]$PROBEID
+  expressonValuesForGene = lumiBatchFrame[lumiBatchFrame$PROBEID %in% probesetidList,]
+  apply(expressonValuesForGene[1:ncol(expressonValuesForGene)-1],2, as.numeric)
+  # labeled vector of average expression values for probesets pointing go a gene for each data set
+  a = apply(expressonValuesForGene[1:ncol(expressonValuesForGene)-1],2, mean)
+  geneToExpressionValueMappingDuplicate[nrow(geneToExpressionValueMappingDuplicate)+1,] <- a
+}
+
+geneToExpressionValueMappingDuplicate$ENTREZID = unique_entrezid
+
+length(colnames(geneToExpressionValueMappingDuplicate))
+
+geneToExpressionValueMapping = rbind(geneToExpressionValueMappingNoDuplicate,geneToExpressionValueMappingDuplicate)
+
+nrow(geneToExpressionValueMapping) # 21041
+# ANSWER
+View(geneToExpressionValueMapping) 
+
+#test ENTREZID is unique in geneToExpressionValueMapping
+duplicate_entrezid = unique(geneToExpressionValueMapping[duplicated(geneToExpressionValueMapping$ENTREZID),]$ENTREZID)
+length(duplicate_entrezid) == 0
 
 
 
@@ -39,7 +84,7 @@ View(probeid_to_gene_mapping)
 gene_frequency_table = table(probeid_to_gene_mapping$ENTREZID)
 # genes of all probeset amounts 
 gene_frequency_table = as.data.frame(gene_frequency_table)
-# genes of probeset amounts > 10
+# genes of probeset amounts > 10 (to make sence of scale)
 filtered_gene_frequency_table = data.frame()
 for (i in 1:nrow(gene_frequency_table)){
   row <- gene_frequency_table[i,]
@@ -51,13 +96,6 @@ for (i in 1:nrow(gene_frequency_table)){
 c <- ggplot(filtered_gene_frequency_table, aes(factor(Freq)))
 c <- ggplot(gene_frequency_table, aes(factor(Freq)))
 c + geom_bar()
-
-lumiBatchFrame <- data.frame(lumiBatchN)
-annot <- data.frame(SYMBOL=sapply(contents(illuminaHumanv4SYMBOL), paste, collapse=", "))
-
-
-all <- merge(annot, lumiBatchFrame, by.x=0, by.y=0, all=T)
-
 
 design <- model.matrix(~0 + colnames(lumiBatchN))
 View(design)
